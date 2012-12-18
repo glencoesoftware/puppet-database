ae4a0639aee3cdb25c99783cbd6b77af30630bba
Chris MacLeod <ckm@glencoesoftware.com>
2012-12-18 11:33:13 -0500

    stub slave and master classes

---
+++ manifests/postgres/replication/master.pp
@@ -0,0 +1,19 @@
+# Class: manifests::postgres::replication::master
+#
+# This class does stuff that you describe here
+#
+# Parameters:
+#   $parameter:
+#       this global variable is used to do things
+#
+# Actions:
+#   Actions should be described here
+#
+# Requires:
+#   - Package["foopackage"]
+#
+# Sample Usage:
+#
+class manifests::postgres::replication::master {
+  notice "master class replication"
+}
---
+++ manifests/postgres/replication/slave.pp
@@ -0,0 +1,19 @@
+# Class: database::postgres::replication::slave
+#
+# This class does stuff that you describe here
+#
+# Parameters:
+#   $parameter:
+#       this global variable is used to do things
+#
+# Actions:
+#   Actions should be described here
+#
+# Requires:
+#   - Package["foopackage"]
+#
+# Sample Usage:
+#
+class database::postgres::replication::slave {
+  notice "slave class replication"
+}
