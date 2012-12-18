8dcbe92a91c3831b583e9eca3b5658a2a31920d9
Chris MacLeod <ckm@glencoesoftware.com>
2012-12-18 12:48:50 -0500

    more replication settings

--- manifests/postgres/replication.pp
+++ manifests/postgres/replication.pp
@@ -15,5 +15,15 @@
 # Sample Usage:
 #
 class database::postgres::replication inherits database::postgres {
-  notice "doing stuff with replication"
+
+  # these should not have defaults so that they fail when not specified
+  $pg_replication_master = hiera_array('postgres_replication_master')
+  $pg_restrore_command = hiera('postgres_restore_command')
+
+  file {
+    'recovery.conf':
+      path    => "/var/lib/pgsql/${version}/data/recovery.conf",
+      content => template("${module_name}/recovery.conf.erb"),
+      ;
+  }
 }
