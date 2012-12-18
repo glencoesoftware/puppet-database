534ea6914c3c8822cba899ac27698120e8214663
Chris MacLeod <ckm@glencoesoftware.com>
2012-12-18 09:53:23 -0500

    stub replication class

---
+++ manifests/postgres/replication.pp
@@ -0,0 +1,19 @@
+# Class: database::postgres::replication
+#
+# This class does stuff that you describe here
+#
+# Parameters:
+#   $parameter:
+#       this global variable is used to do things
+#
+# Actions:
+#   Actions should be described here
+#
+# Requires:
+#   - Package["foopackage"]
+#
+# Sample Usage:
+#
+class database::postgres::replication {
+  notice "doing stuff with replication"
+}
