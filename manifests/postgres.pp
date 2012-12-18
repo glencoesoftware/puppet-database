d90cd336f8c35dbe28fee50a8d4a3b3c86e3d1d6
Chris MacLeod <ckm@glencoesoftware.com>
2012-12-18 12:09:44 -0500

    initial stab at replication setup

--- manifests/postgres.pp
+++ manifests/postgres.pp
@@ -3,6 +3,16 @@ class database::postgres (
   $release_pkg_name = hiera('postgres_release_package', 'pgdg-centos91'),
   $pg_user = hiera('postgres_user', 'postgres'),
   $service_name = hiera('postgres_custom_service_name', ''),
+  $pg_listen_addresses = hiera('postgres_listen_address', 'localhost'),
+  $pg_max_connections = hiera('postgres_max_connections', '100'),
+  $pg_wal_level = hiera('postgres_wal_level', 'minimal'),
+  $pg_max_wal_senders = hiera('postgres_max_wal_senders', '0'),
+  $pg_wal_keep_segments = hiera('postgres_wal_keep_segments', '0'),
+  $pg_archive_mode = hiera('postgres_archive_mode', 'off'),
+  $pg_archive_command = hiera('postgres_archive_command', ''),
+  $pg_hot_standby = hiera('postgres_hot_standby', 'off'),
+  $pg_replication_slave = hiera('postgres_replication_slave', ''),
+  $pg_replication_master = hiera('postgres_replication_master', ''),
 ) {
   
   # install classes
@@ -42,11 +52,7 @@ class database::postgres (
       ;
     "postgresql.conf":
       path    => "/var/lib/pgsql/${version}/data/postgresql.conf",
-      # content => template("${module_name}/postgresql.conf.erb"),
-      source  => [ 
-        "puppet:///modules/${module_name}/postgres/postgresql.conf.${fqdn}",
-        "puppet:///modules/${module_name}/postgres/postgresql.conf",
-      ],
+      content => template("${module_name}/postgresql.conf.erb"),
       require => Exec['initdb'],
       notify  => Service['postgres'],
       mode    => '0600',
