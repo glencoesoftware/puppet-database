75c2076570a664d1b109aaf3100f6111417b723d
Chris MacLeod <ckm@glencoesoftware.com>
2012-12-19 14:39:01 -0500

    I'm dumb

--- manifests/postgres/replication.pp
+++ manifests/postgres/replication.pp
@@ -18,21 +18,18 @@ class database::postgres::replication inherits database::postgres {
   # generic replication settings go here
   # replication dbs need to have keys
   file {
-    "${vardir}/.ssh":
+    "/var/lib/pgsql/.ssh":
       ensure => 'directory',
       mode   => '0700',
       ;
     'ssh_private_key':
-      path   => "${vardir}/.ssh/id_rsa",
+      path   => "/var/lib/pgsql/.ssh/id_rsa",
       mode   => '0600',
       source => "puppet:///modules/${module_name}/postgres-ssh-privatekey.key",
       ;
   }
 
   $pg_ssh_pubkey = hiera('postgres_ssh_pubkey')
-  notice "pguser: ${pg_user}"
-  notice "key: ${pg_ssh_pubkey}"
-
   ssh_authorized_key { 'ssh_key_postgres':
     ensure => 'present',
     user   => $pg_user,
