0b8d78572685de5618c9b7ca32fb21fd4a9e5a5b
Chris MacLeod <ckm@glencoesoftware.com>
2012-12-19 16:19:16 -0500

    we don't always need this

--- manifests/postgres/replication/slave.pp
+++ manifests/postgres/replication/slave.pp
@@ -15,9 +15,10 @@
 # Sample Usage:
 #
 class database::postgres::replication::slave inherits database::postgres {
+  $pg_restore_command = hiera('postgres_restore_command', '')
+
   # these should not have defaults so that they fail when not specified
   $pg_replication_master = hiera_hash('postgres_replication_master')
-  $pg_restore_command = hiera('postgres_restore_command')
 
   file {
     'recovery.conf':
